827e0dd8ca984c809266eedc3b370dae2215887a
Thomas Guillerme <guillert@tcd.ie>
2017-03-15 11:25:33 +0000

    Added 6th matrix to the 12t tree for testing routines

--- test/test-plot.algorithm.R
+++ test/test-plot.algorithm.R
@@ -163,7 +163,7 @@ matrix3_2 <- make.states.matrix(tree3, character3_2)
 matrix3_3 <- make.states.matrix(tree3, character3_3)
 matrix3_4 <- make.states.matrix(tree3, character3_4)
 matrix3_5 <- make.states.matrix(tree3, character3_5)
-# matrix3_6 <- make.states.matrix(tree3, character3_6)
+matrix3_6 <- make.states.matrix(tree3, character3_6)
 
 ## Running the pass
 pass1_1 <- first.downpass(matrix1, tree1)
@@ -173,7 +173,7 @@ pass1_3_2 <- first.downpass(matrix3_2, tree3)
 pass1_3_3 <- first.downpass(matrix3_3, tree3)
 pass1_3_4 <- first.downpass(matrix3_4, tree3)
 pass1_3_5 <- first.downpass(matrix3_5, tree3)
-# pass1_3_6 <- first.downpass(matrix3_6, tree3)
+pass1_3_6 <- first.downpass(matrix3_6, tree3)
 
 
 
@@ -186,6 +186,7 @@ test_that("first.downpass works", {
     results3_3_inapp1 <- list(c(-1),c(-1),c(-1,0),c(0),c(0,1),c(1),c(-1),c(-1,0),c(0),c(0,1),c(1)) ## The 8th node is c(-1,1) in morphy! 9th is c(1) in morphy! 
     results3_4_inapp1 <- list(c(-1),c(-1),c(-1,1),c(-1),c(-1,2,3),c(2,3),c(-1),c(-1),c(-1,0,2,3),c(0,2,3),c(2,3))
     results3_5_inapp1 <- list(c(1),c(-1,1),c(-1),c(-1),c(-1,0,1),c(0,1),c(0,1),c(1),c(0,1),c(1),c(0,1))
+    results3_6_inapp1 <- list(c(0,1),c(-1,1),c(-1),c(-1,0,1,2),c(0,1,2),c(1,2),c(0),c(-1,0),c(-1),c(-1,1,2),c(1,2))
 
     ## Testing tree 2
     for(node in 1:7) {
@@ -207,6 +208,7 @@ test_that("first.downpass works", {
         expect_equal(pass1_3_3[[2]][node+12], results3_3_inapp1[node])
         expect_equal(pass1_3_4[[2]][node+12], results3_4_inapp1[node])
         expect_equal(pass1_3_5[[2]][node+12], results3_5_inapp1[node])
+        expect_equal(pass1_3_6[[2]][node+12], results3_6_inapp1[node])
     }
 })
 
@@ -218,6 +220,7 @@ pass2_3_2 <- first.uppass(pass1_3_2, tree3)
 pass2_3_3 <- first.uppass(pass1_3_3, tree3)
 pass2_3_4 <- first.uppass(pass1_3_4, tree3)
 pass2_3_5 <- first.uppass(pass1_3_5, tree3)
+pass2_3_6 <- first.uppass(pass1_3_6, tree3)
 
 context("first.uppass")
 test_that("first.uppass works", {
@@ -228,6 +231,7 @@ test_that("first.uppass works", {
     results3_3_inapp2 <- list(c(-1),c(-1),c(-1),c(0),c(0,1),c(1),c(-1),c(-1),c(0),c(0,1),c(1)) # 9th is c(1) in morphy! 
     results3_4_inapp2 <- list(c(-1),c(-1),c(-1),c(-1),c(-1),c(2,3),c(-1),c(-1),c(-1),c(0,2,3),c(2,3))
     results3_5_inapp2 <- list(c(1),c(1),c(-1),c(-1),c(-1),c(0,1),c(0,1),c(1),c(0,1),c(1),c(0,1))
+    results3_6_inapp2 <- list(c(0,1),c(1),c(0,1,2),c(0,1,2),c(0,1,2),c(1,2),c(0),c(0),c(1,2),c(1,2),c(1,2))
 
     ## Testing tree 2
     for(node in 1:7) {
@@ -249,6 +253,7 @@ test_that("first.uppass works", {
         expect_equal(pass2_3_3[[3]][node+12], results3_3_inapp2[node])
         expect_equal(pass2_3_4[[3]][node+12], results3_4_inapp2[node])
         expect_equal(pass2_3_5[[3]][node+12], results3_5_inapp2[node])
+        expect_equal(pass2_3_6[[3]][node+12], results3_6_inapp2[node])
     }
 
 })
@@ -261,6 +266,7 @@ pass3_3_2 <- second.downpass(pass2_3_2, tree3)
 pass3_3_3 <- second.downpass(pass2_3_3, tree3)
 pass3_3_4 <- second.downpass(pass2_3_4, tree3)
 pass3_3_5 <- second.downpass(pass2_3_5, tree3)
+pass3_3_6 <- second.downpass(pass2_3_6, tree3)
 
 context("second.downpass")
 test_that("second.downpass works", {
@@ -271,6 +277,7 @@ test_that("second.downpass works", {
     results3_3_inapp3 <- list(c(-1),c(-1),c(-1),c(0),c(0,1),c(1),c(-1),c(-1),c(0),c(0,1),c(1)) # 9th is c(1) in morphy! 
     results3_4_inapp3 <- list(c(-1),c(-1),c(-1),c(-1),c(-1),c(2,3),c(-1),c(-1),c(-1),c(0,2,3),c(2,3))
     results3_5_inapp3 <- list(c(1),c(1),c(-1),c(-1),c(-1),c(0,1),c(0,1),c(1),c(0,1),c(1),c(0,1))
+    results3_6_inapp3 <- list(c(0,1),c(1),c(0,1,2),c(0,1,2),c(0,1,2),c(1,2),c(0),c(0,1,2),c(1,2),c(1,2),c(1,2))
 
     ## Testing tree 2
     for(node in 1:7) {
@@ -292,6 +299,7 @@ test_that("second.downpass works", {
         expect_equal(pass3_3_3[[4]][node+12], results3_3_inapp3[node])
         expect_equal(pass3_3_4[[4]][node+12], results3_4_inapp3[node])
         expect_equal(pass3_3_5[[4]][node+12], results3_5_inapp3[node])
+        expect_equal(pass3_3_6[[4]][node+12], results3_6_inapp3[node])
     }
 
 })
@@ -304,6 +312,7 @@ pass4_3_2 <- second.uppass(pass3_3_2, tree3)
 pass4_3_3 <- second.uppass(pass3_3_3, tree3)
 pass4_3_4 <- second.uppass(pass3_3_4, tree3)
 pass4_3_5 <- second.uppass(pass3_3_5, tree3)
+pass4_3_6 <- second.uppass(pass3_3_6, tree3)
 
 context("second.uppass")
 test_that("second.uppass works", {
@@ -314,6 +323,7 @@ test_that("second.uppass works", {
     results3_3_inapp4 <- list(c(-1),c(-1),c(-1),c(0),c(0),c(1),c(-1),c(-1),c(0),c(0),c(1))  # 9th and 10th is c(1) in morphy! 
     results3_4_inapp4 <- list(c(-1),c(-1),c(-1),c(-1),c(-1),c(2,3),c(-1),c(-1),c(-1),c(0,2,3),c(0,2,3))
     results3_5_inapp4 <- list(c(1),c(1),c(-1),c(-1),c(-1),c(0,1),c(1),c(1),c(1),c(1),c(1))
+    results3_6_inapp4 <- list(c(0,1),c(0,1),c(0,1),c(0,1),c(0,1),c(0,1,2),c(0,1),c(0,1),c(0,1),c(0,1),c(0,1,2))
 
     ## Testing tree 2
     for(node in 1:7) {
@@ -335,6 +345,7 @@ test_that("second.uppass works", {
         expect_equal(pass4_3_3[[5]][node+12], results3_3_inapp4[node])
         expect_equal(pass4_3_4[[5]][node+12], results3_4_inapp4[node])
         expect_equal(pass4_3_5[[5]][node+12], results3_5_inapp4[node])
+        expect_equal(pass4_3_6[[5]][node+12], results3_6_inapp4[node])
     }
 })
 

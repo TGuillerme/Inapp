984ccfb15039cd52a3c4fc9dadf62355effa291f
Thomas Guillerme <guillert@tcd.ie>
2017-04-28 15:43:38 +0100

    Output handle INIT

---
+++ R/output.results.R
@@ -0,0 +1,28 @@
+# ' @title Outputs results
+# '
+# ' @description Outputs ancestral states reconstruction results
+# '
+# ' @param states_matrix A \code{states.matrix} list from \code{\link{apply.reconstruction}}
+# ' @param output The type of output
+# ' # Set options here
+# ' 
+# ' @examples
+# ' 
+# ' @seealso \code{\link{apply.reconstruction}}, \code{\link{plot.states.matrix}}
+# ' 
+# ' @author Thomas Guillerme
+# ' @export
+
+# output.states.matrix <- function(states_matrix, ouput, ...) {
+#     ## Write nexus with node comments
+
+#     ## Node data stucture:
+#     #[Dp1:,Up1:,Dp2:,Up2:,Change,Applicable]
+
+#     # ((a[Dp1:,Up1:,Dp2:,Up2:,Change,Applicable],b[])[], (c[],d[]));
+
+
+#     ## Write output in xml
+
+#     ## Write output in pdf
+# }

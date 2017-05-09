9659d22dcabd1b41f65a4042b997eeab4c8a7793
Thomas Guillerme <guillert@tcd.ie>
2017-05-09 15:06:36 +0100

    Added analysis functions (not part of the package)

---
+++ Analysis/Functions/read.tree.score.R
@@ -0,0 +1,33 @@
+#' @title Read tree scores
+#'
+#' @description Reads a list of tree lengths
+#'
+#' @param chain The chain name
+#' @param path The path to the file to be read
+#' 
+#' @examples
+#'
+#' @seealso
+#' 
+#' @author Thomas Guillerme
+#' @export
+
+read.tree.score <- function(chain, path = ".") {
+
+    chain_list <- list.files(path, pattern = chain)
+    full_path <- paste(path, chain_list, sep = "")
+    names <- strsplit(chain_list, split = paste(chain, ".", sep = ""))
+    names <- unlist(lapply(names, `[[`, 2))
+    names <- strsplit(names, split = ".score")
+    names <- unlist(lapply(names, `[[`, 1))
+
+
+    data <- list()
+    for(file in 1:length(chain_list)) {
+        data[[file]] <- read.table(full_path[[file]], header = TRUE)[,2]
+    }
+
+    names(data) <- names
+
+    return(data)
+}
---
+++ Analysis/Functions/standardise.score.R
@@ -0,0 +1,26 @@
+#' @title Standardise scores
+#'
+#' @description Standardises parsimony scores
+#'
+#' @param data A list of parsimony scores
+#' @param std The standardisation type
+#' 
+#' @examples
+#'
+#' @seealso
+#' 
+#' @author Thomas Guillerme
+#' @export
+
+standardise.score <- function(data, std = "base") {
+
+    if(std == "base") {
+        ## Standardise the scores by dividing it by the base score
+        data_out <- list()
+        for(score in 1:length(data)) {
+            data_out[[score]] <- data[[score]]/data$NAmiss-1
+        }
+        names(data_out) <- names(data)
+        return(data_out)
+    }
+}

63e94f74ad1f00ef306835252b271ae7578d891a
ms609 <martins@gmail.com>
2018-01-16 13:58:01 +0000

    Revert "Update reference"
    This reverts commit 11aaf5a59ff62befea25272ba744d343be2a0e64.

--- R/Inapp-package.R
+++ R/Inapp-package.R
@@ -12,8 +12,7 @@
 #'
 #' @references
 #'
-#' Brazeau, M. D., Guillerme, T., and Smith, M. R. 2017. Morphological 
-#' phylogenetic analysis with inapplicable data. Biorxiv. <http://doi.org/10.1101/209775>
+#' Smith, M. R., Brazeau, M. D., and Guillerme, T., 20??. @@@@
 #' 
 #' @keywords maximum parsimony, inapplicable characters, states reconstruction, tree length
 #'

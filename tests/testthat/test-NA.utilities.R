52d404ba8a5a4b911fb5838ba94c8be665b66a9f
ms609 <martins@gmail.com>
2018-01-17 13:50:16 +0000

    Make test less opaque

--- tests/testthat/test-NA.utilities.R
+++ tests/testthat/test-NA.utilities.R
@@ -49,7 +49,7 @@ test_that("make.states.matrix works", {
     expect_warning(matrix <- make.states.matrix(tree, character))  # Warning is some weird NA management by testthat
     expect_is(matrix, "states.matrix")
     expect_equal(unique(unlist(lapply(matrix, class))), c("list", "integer", "phylo"))
-    expect_equal(unique(unlist(lapply(matrix, length))), c(7,4,1,0,3))
+    expect_equal(as.integer(lapply(matrix, length)), c(7,7,7,7,7,4,0,0,0,3,1,1))
     expect_equal(length(matrix), 12)
     expect_equal(names(matrix), c("Char", "Dp1", "Up1", "Dp2", "Up2", "tracker", "regions", "changes", "length",  "tree", "n_tip", "n_node"))
     expect_equal(ape::Ntip(matrix$tree), matrix$n_tip)

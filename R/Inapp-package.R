a6c2cdd3c1ce3e97096748a68809590a84f39668
ms609 <martins@gmail.com>
2018-01-25 11:03:51 +0000

    Documentation modifications to fix WARNINGs

--- R/Inapp-package.R
+++ R/Inapp-package.R
@@ -1,7 +1,7 @@
 #' Inapplicable Discrete Characters Reconstruction
-#' 
+#'
 #' An algorithm allowing states reconstruction and tree score calculation for inapplicable discrete characters
-#' 
+#'
 #' @name Inapp-package
 #'
 #' @aliases Inapp
@@ -12,13 +12,16 @@
 #'
 #' @references
 #'
-#' Brazeau, M. D., Guillerme, T., and Smith, M. R. 2017. Morphological 
+#' Brazeau, M. D., Guillerme, T., and Smith, M. R. 2017. Morphological
 #' phylogenetic analysis with inapplicable data. Biorxiv. <https://doi.org/10.1101/209775>
 #'
 #' @keywords maximum parsimony, inapplicable characters, states reconstruction, tree score
 #'
 #' @examples
-#' 
+#' \dontRun{
+#' runInapp()
+#' }
+#'
 #' @exportPattern "^[[:alpha:]]+"
 #' @import ape
 #' @importFrom graphics plot legend

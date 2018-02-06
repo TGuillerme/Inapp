366dae0fe8ca33a02f7f9de237f8e5e697c46419
ms609 <martins@gmail.com>
2018-02-06 15:09:31 +0000

    Option to not label nodes

--- R/vignettePlots.R
+++ R/vignettePlots.R
@@ -13,14 +13,15 @@
 #' @keywords Internal
 #'
 #' @export
-vignettePlot <- function (tree, character, na=TRUE, legend.pos='bottomleft', ...) {
+vignettePlot <- function (tree, character, na=TRUE, legend.pos='bottomleft',
+                          blankNodes=FALSE, ...) {
   tree$edge.length <- rep(1, dim(tree$edge)[1])
   reconstruction <- apply.reconstruction(tree, character,
                                          method=if(na) "NA" else 'Fitch',
                                          match.tip.char=TRUE)
   dev.new(); plot(tree, direction='upwards', ...); corners <- par('usr'); dev.off()
   plot(reconstruction,
-       passes=if(na) 1:4 else 1:2, counts=if(na) 1:2 else 2,
+       passes=if(blankNodes) 0 else if(na) 1:4 else 1:2, counts=if(na) 1:2 else 2,
        direction='upwards', legend.pos=legend.pos,
        col.states=TRUE, use.edge.length=TRUE,
        x.lim=c(-3, corners[2]), y.lim=c(-1, corners[4]+0.1), ...)

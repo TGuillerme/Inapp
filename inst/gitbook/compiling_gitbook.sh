398a78316c08e2a9763b3a2dc5eab753ee744883
Thomas Guillerme <guillert@tcd.ie>
2018-03-05 16:42:26 +1000

    Book down update

--- inst/gitbook/compiling_gitbook.sh
+++ inst/gitbook/compiling_gitbook.sh
@@ -3,15 +3,15 @@
 ## Compile the html
 R -e 'bookdown::render_book("index.Rmd", "bookdown::gitbook")' # HTML version
 
-## Comment out the badges
-sed 's/<!-- badges out start -->/<!-- badges out start/g' index.Rmd > index.tmp
-sed 's/<!-- badges out end   -->/badges out end   -->/g' index.tmp > index.Rmd
-rm index.tmp
+# ## Comment out the badges
+# sed 's/<!-- badges out start -->/<!-- badges out start/g' index.Rmd > index.tmp
+# sed 's/<!-- badges out end   -->/badges out end   -->/g' index.tmp > index.Rmd
+# rm index.tmp
 
-## Compile the pdf
-R -e 'bookdown::render_book("index.Rmd", "bookdown::pdf_book")' # PDF version
+# ## Compile the pdf
+# R -e 'bookdown::render_book("index.Rmd", "bookdown::pdf_book")' # PDF version
 
-## Reactivate the badges
-sed 's/<!-- badges out start/<!-- badges out start -->/g' index.Rmd > index.tmp
-sed 's/badges out end   -->/<!-- badges out end   -->/g' index.tmp > index.Rmd
-rm index.tmp
+# ## Reactivate the badges
+# sed 's/<!-- badges out start/<!-- badges out start -->/g' index.Rmd > index.tmp
+# sed 's/badges out end   -->/<!-- badges out end   -->/g' index.tmp > index.Rmd
+# rm index.tmp

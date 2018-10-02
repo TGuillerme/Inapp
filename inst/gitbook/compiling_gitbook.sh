85c6df5c04a4e8664c7fc681782fa4ebd0bc04a5
Martin Smith <martins@gmail.com>
2018-10-02 15:54:06 +0100

    Don't render a PDF
    Figure layout is problematic, and probably not worth fixing

--- inst/gitbook/compiling_gitbook.sh
+++ inst/gitbook/compiling_gitbook.sh
@@ -3,4 +3,6 @@
 ## Compile the html
 R -e 'bookdown::render_book("index.Rmd", "bookdown::gitbook")' # HTML version
 ## Compile the pdf
-R -e 'bookdown::render_book("index.Rmd", "bookdown::pdf_book")' # PDF version
+##   We do not presently compile a PDF as there are issues with the
+##   layout and position of figures
+## R -e 'bookdown::render_book("index.Rmd", "bookdown::pdf_book")' # PDF version

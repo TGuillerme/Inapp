5668a2f656530316c2acde242581458a7dd4bfcd
TGuillerme <guillert@tcd.ie>
2018-10-04 16:25:14 +1000

    coverage up for apply.reconstruction

--- tests/testthat/test-apply.reconstruction.R
+++ tests/testthat/test-apply.reconstruction.R
@@ -1,4 +1,12 @@
-# context("inapplicable.algorithm")
-# test_that("inapplicable.algorithm works", {
-#     ## Test the three cases
-# })
+context("inapplicable.algorithm")
+test_that("inapplicable.algorithm works", {
+    
+
+    ## Random tree with 12 taxa
+    tree <- ape::rtree(12, br = NULL)
+    ## A character with inapplicable data
+    character <- "23--1??--032"
+    ## Normal Fitch algorithm (NA states are missing data)
+    expect_error(apply.reconstruction(tree, character, passes = 2, method = "Futch", inapplicable = 1))
+    expect_error(apply.reconstruction(tree, character, passes = 2, method = "Fitch", inapplicable = 3))
+})

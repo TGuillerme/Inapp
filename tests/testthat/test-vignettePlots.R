d2113216db38de875b1472513d8f4852161b8dca
TGuillerme <guillert@tcd.ie>
2018-10-04 16:39:20 +1000

    coverage for vignettePlots

---
+++ tests/testthat/test-vignettePlots.R
@@ -0,0 +1,15 @@
+context("vignettePlots and svg")
+
+## Test
+test_that("vignettePlots works", {
+
+    ## Random vignette tests
+    hasTail   <- ape::read.tree(text="((a, (b, (c, d))), (e, ((f, (g, X)), (h, i))));")
+    tail <- "000001111?"
+    ap <- c("Absent", "Present")
+    expect_null(vignettePlot(hasTail, tail, legend.pos='topleft', na=FALSE, main="Tail", state.labels = ap))
+    rb <- c('Red', 'Blue')
+    orb <- c('', rb)
+    expect_null(vignettePlot(ape::read.tree(text="((a, (i1, i2)), (b, (c, (d, (e, (f, (g, h)))))));"),
+             '---11122??', legend.pos='topleft', state.labels = orb, passes=4))
+})

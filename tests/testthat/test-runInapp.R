9120ea09757a6f4a32c8ce988528e55bd19bb102
TGuillerme <guillert@tcd.ie>
2018-10-05 10:39:28 +1000

    unInapp coverage increased

--- tests/testthat/test-runInapp.R
+++ tests/testthat/test-runInapp.R
@@ -5,4 +5,11 @@ test_that("RunInapp works", {
 
     ## Sanitizing
     expect_error(runInapp(remote = "blue"))
+
+    ## Expect running (but not running)
+    msg <- capture_messages(expect_warning(expect_error(runInapp(remote = TRUE, ref = "doesntexist"))))
+    expect_equal(msg, "Downloading https://github.com/TGuillerme/Inapp/archive/doesntexist.tar.gz\n")
+
+    ## Expect running (but not running)
+    expect_error(runInapp(remote = FALSE, path = "dummy"))
 })

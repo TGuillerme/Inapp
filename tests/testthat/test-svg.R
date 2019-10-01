aa74679ef7b4389ac9b80e4d783cebe66e475324
Martin Smith <martins@gmail.com>
2019-10-01 12:56:41 +0100

    Test SVG plotting

--- tests/testthat/test-svg.R
+++ tests/testthat/test-svg.R
@@ -1,96 +1,27 @@
-# context("svg functions")
+context("svg functions")
+library('ape')
 
-# ## Test
-# test_that("SVGCanvas works", {
+test_that('SVG can be plotted', {
+    trees <- structure(list(
+      tree1 = read.tree(text='((a, b), ((c, (d, (e, f))), ((g, h), (i, j))));'),
+      tree2 = read.tree(text='((a, b), ((c, (d, (e, f))), ((g, i), (h, j))));'),
+      tree3 = read.tree(text='(a, (b, ((c, (d, (e, h))), ((g, f), (i, j)))));'),
+      tree1again = read.tree(text='((a, b), ((c, (d, (e, f))), ((g, h), (i, j))));')),
+      class='multiPhylo')
 
-#     ## Sanitizing
-#     expect_error(FUN@@@())
+    treesGeneratedBy <- c('text', 'text', 'text', 'duplication')
 
-#     ## Right output
-#     expect_is(
-#         FUN@@@()
-#         , "class")
-#     expect_equal(
-#         dim(FUN@@@())
-#         , dim)
-# })
+    canvas <- SVGCanvas(trees=trees,
+                     outgroupTips=c('a', 'b'),
+                     analysisNames=treesGeneratedBy,
+                     width=300, height=300)
 
-
-# ## Test
-# test_that("SVGTree works", {
-
-#     ## Sanitizing
-#     expect_error(FUN@@@())
-
-#     ## Right output
-#     expect_is(
-#         FUN@@@()
-#         , "class")
-#     expect_equal(
-#         dim(FUN@@@())
-#         , dim)
-# })
-
-
-# ## Test
-# test_that("MatrixData works", {
-
-#     ## Sanitizing
-#     expect_error(FUN@@@())
-
-#     ## Right output
-#     expect_is(
-#         FUN@@@()
-#         , "class")
-#     expect_equal(
-#         dim(FUN@@@())
-#         , dim)
-# })
-
-
-# ## Test
-# test_that("PlotCharacterMapping works", {
-
-#     ## Sanitizing
-#     expect_error(FUN@@@())
-
-#     ## Right output
-#     expect_is(
-#         FUN@@@()
-#         , "class")
-#     expect_equal(
-#         dim(FUN@@@())
-#         , dim)
-# })
-
-
-# ## Test
-# test_that("PrintSwitcher works", {
-
-#     ## Sanitizing
-#     expect_error(FUN@@@())
-
-#     ## Right output
-#     expect_is(
-#         FUN@@@()
-#         , "class")
-#     expect_equal(
-#         dim(FUN@@@())
-#         , dim)
-# })
-
-
-# ## Test
-# test_that("PrintJavascript works", {
-
-#     ## Sanitizing
-#     expect_error(FUN@@@())
-
-#     ## Right output
-#     expect_is(
-#         FUN@@@()
-#         , "class")
-#     expect_equal(
-#         dim(FUN@@@())
-#         , dim)
-# })
+    PlotExample <- function ()
+    PlotCharacterMapping(char = '0011----11',
+                      stateLabels = c('Absent', 'Present'),
+                      singleTree = trees[[1]],
+                      legendText = 'This is printed on PNGs',
+                      canvas = canvas,
+                      svgFilename = 'tree_number_%s.svg')
+  expect_doppelganger('SVG example', PlotExample)
+})

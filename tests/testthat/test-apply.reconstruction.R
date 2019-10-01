47a53d6bff18dbeca932caf621c617cab3e57291
Martin Smith <martins@gmail.com>
2019-10-01 12:42:14 +0100

    Test no applicables

--- tests/testthat/test-apply.reconstruction.R
+++ tests/testthat/test-apply.reconstruction.R
@@ -1,7 +1,5 @@
 context("inapplicable.algorithm")
 test_that("inapplicable.algorithm works", {
-    
-
     ## Random tree with 12 taxa
     tree <- ape::rtree(12, br = NULL)
     ## A character with inapplicable data
@@ -9,4 +7,10 @@ test_that("inapplicable.algorithm works", {
     ## Normal Fitch algorithm (NA states are missing data)
     expect_error(apply.reconstruction(tree, character, passes = 2, method = "Futch", inapplicable = 1))
     expect_error(apply.reconstruction(tree, character, passes = 2, method = "Fitch", inapplicable = 3))
+    recon <- apply.reconstruction(tree, character, passes = 4, method = "NA", inapplicable = 1)
+    expect_false(is.null(recon$Up2[[23]]))
+
+    recon <- apply.reconstruction(tree, '------???---', method = "Fitch",
+                                  inapplicable = 1)
+
 })

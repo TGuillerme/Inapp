fc2bf8d62e5e2241aef02a8ff49e27c6f421e377
Martin Smith <martins@gmail.com>
2020-12-01 09:22:55 +0000

    Imports & suggests

--- R/runInapp.R
+++ R/runInapp.R
@@ -4,16 +4,17 @@
 #'
 #' @param ... Any optional option to be passed to shiny::runApp
 #' @param remote Whether to run the app from a remote server (\code{TRUE}, default - \code{shiny::runGitHub("Inapp", "TGuillerme", ...)}) or from the local server (\code{FALSE} - \code{shiny::runApp(".", ...)}).
-#' 
+#'
 #' @examples
 #' \dontrun{
 #' ## Running the GUI on your web browser
 #' runInapp()
 #' }
-#' 
+#'
 #' @seealso \code{\link{apply.reconstruction}}, \code{\link{plot.states.matrix}}
-#' 
+#'
 #' @author Thomas Guillerme
+#' @importFrom TreeTools ReadCharacters
 #' @export
 
 runInapp <- function(..., remote = TRUE) {
